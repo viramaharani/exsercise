cd5e4352c1eedfc593c2f9eddc8fcb039576f1d9
viramaharani <viraharun0612@gmail.com>
2017-10-25 08:48:30 +0700

    latihan

---
+++ HelloTestJava.java
@@ -0,0 +1,15 @@
+
+public class HelloTestJava {
+	
+
+//Sesuaikan nama file dengan nama class
+
+public static void main(String[] args) {
+	
+
+     System.out.println("Belajar java di okedroid.com");
+
+}
+
+
+}
